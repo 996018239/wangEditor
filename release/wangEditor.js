76b5ccb439546813cf6e5dd28fc4124e39d75f90
996018239 <996018239@qq.com>
2020-02-14 17:59:21 +0800

    Update wangEditor.js

--- release/wangEditor.js
+++ release/wangEditor.js
@@ -1377,10 +1377,6 @@ Link.prototype = {
 
     // 创建 panel
     _createPanel: function _createPanel(text, link) {
-if(typeof(this.editor.customConfig.createLink) == 'function'){
-            this.editor.customConfig.createLink()
-            return;
-        }
         var _this = this;
 
         // panel 中需要用到的id
